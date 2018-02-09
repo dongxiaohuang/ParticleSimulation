e4c153ecc564030a795c1f50fc2992ac5435b8ff
Lin Li <lil@firefligts.cn>
2018-02-09 12:54:01 +0000

    Add & remove - PQ

--- src/utils/MinPriorityQueue.java
+++ src/utils/MinPriorityQueue.java
@@ -3,13 +3,14 @@ package utils;
 public class MinPriorityQueue<T extends Comparable<T>> {
 
     private static int CAPACITY = 1000;
+    private static int ROOT_INDEX = 1
     
     private double[] tree;
-    private int index;
+    private int blank_index;
     
     public MinPriorityQueue() {
-	tree = new double[CAPACITY];
-	index = 1
+	this.tree = new double[CAPACITY];
+	this.blank_index = ROOT_INDEX
     }
 
     /**
@@ -24,17 +25,66 @@ public class MinPriorityQueue<T extends Comparable<T>> {
      * Adds elem to the queue.
      */
     public void add(T elem) {
-        // TODO implement this method
-	tree[index] = elem;
-	boolean finished = true;
+	tree[blank_index] = elem;
+	
+	boolean finished = false;
+	int parent_index = blank_index / 2;
+	int child_index = blank_index;
+	while (!finished) {
+	    if (parent_index == 0)
+		finished = true;
+	    else {
+		if (tree[parent_index].compareTo(elem) < 0) {
+		    T temp = tree[child_index];
+		    tree[child_index] = tree[parent_index];
+		    tree[parent_index] = temp;
+
+		    child_index = parent_index;
+		    parent_index = parent_index / 2;
+		}
+		else
+		    finished = true;
+	    }
+	}
+
+	blank_index += 1;
     }
 
     /**
      * Removes, and returns, the element at the front of the queue.
      */
     public T remove() {
-        // TODO implement this method
-        return null;
+	if (blank_index <= ROOT_INDEX)
+	    return null;
+
+	T removed = tree[ROOT_INDEX);
+	tree[ROOT_INDEX] = tree[blank_index-1];
+	
+	boolean finished = false;
+	int parent_index = ROOT_INDEX;
+	int child_index = 2 * parent_index;
+	while (!finished) {
+	    if (child_index >= blank_index)
+		finished = true;
+	    else {
+		T max_child = tree[child_index];
+		int c_neigh_index = child_index + 1;
+		if (c_neigh_index<blank_index && max_child.compareTo(tree[c_neigh_index])<0) {
+		    max_child = tree[c_neigh_index];
+		    child_index = c_neigh_index;
+		}
+
+		if (max_child.compareTo(tree[parent_index]) > 0) {
+		    tree[child_index] = tree[parent_index];
+		    tree[parent_index] = max_child;
+		    parent_index = child_index;
+		    child_index = parent_index * 2;
+		}
+		else
+		    finished = true;
+	    }
+	}
+	    
     }
 
     /**
